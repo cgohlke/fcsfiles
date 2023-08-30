d52448dddefe5baeba3f39e4769c01f7eeda4ee4
Christoph Gohlke <cgohlke@cgohlke.com>
2023-08-30 13:30:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,37 +16,39 @@ def search(pattern, code, flags=0):
     return match.groups()[0]
 
 
-with open('fcsfiles/fcsfiles.py') as fh:
-    code = fh.read()
+with open('fcsfiles/fcsfiles.py', encoding='utf-8') as fh:
+    code = fh.read().replace('\r\n', '\n').replace('\r', '\n')
 
-version = search(r"__version__ = '(.*?)'", code)
+version = search(r"__version__ = '(.*?)'", code).replace('.x.x', '.dev0')
 
 description = search(r'"""(.*)\.(?:\r\n|\r|\n)', code)
 
 readme = search(
-    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}[__version__|from]',
+    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}from __future__',
     code,
     re.MULTILINE | re.DOTALL,
 )
-
 readme = '\n'.join(
     [description, '=' * len(description)] + readme.splitlines()[1:]
 )
 
-license = search(
-    r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
-    code,
-    re.MULTILINE | re.DOTALL,
-)
-
-license = license.replace('# ', '').replace('#', '')
-
 if 'sdist' in sys.argv:
-    with open('LICENSE', 'w') as fh:
+    # update README and LICENSE files
+
+    with open('README.rst', 'w', encoding='utf-8') as fh:
+        fh.write(readme)
+
+    license = search(
+        r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
+        code,
+        re.MULTILINE | re.DOTALL,
+    )
+    license = license.replace('# ', '').replace('#', '')
+
+    with open('LICENSE', 'w', encoding='utf-8') as fh:
         fh.write('BSD 3-Clause License\n\n')
         fh.write(license)
-    with open('README.rst', 'w') as fh:
-        fh.write(readme)
+
 
 setup(
     name='fcsfiles',
@@ -54,6 +56,7 @@ setup(
     license='BSD',
     description=description,
     long_description=readme,
+    long_description_content_type='text/x-rst',
     author='Christoph Gohlke',
     author_email='cgohlke@cgohlke.com',
     url='https://www.cgohlke.com',
@@ -63,8 +66,9 @@ setup(
         # 'Documentation': 'https://',
     },
     packages=['fcsfiles'],
-    python_requires='>=3.8',
-    install_requires=['numpy>=1.19.2'],
+    package_data={'fcsfiles': ['py.typed']},
+    python_requires='>=3.9',
+    install_requires=['numpy'],
     platforms=['any'],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -73,9 +77,9 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
